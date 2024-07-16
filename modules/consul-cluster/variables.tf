c09bf1ef76bd200ca04a77c6536e25ae809cdb84
vidalgithub <88409846+vidalgithub@users.noreply.github.com>
2024-07-16 01:37:42 -0500

    Update variables.tf

--- modules/consul-cluster/variables.tf
+++ modules/consul-cluster/variables.tf
@@ -276,7 +276,7 @@ variable "ssh_port" {
 
 variable "tags" {
   description = "List of extra tag blocks added to the autoscaling group configuration. Each element in the list is a map containing keys 'key', 'value', and 'propagate_at_launch' mapped to the respective values."
-  type        = list(object({ key : string, value : string, propagate_at_launch : bool }))
+  type        = list(object({ key = string, value = string, propagate_at_launch = bool }))
   default     = []
 }
 
